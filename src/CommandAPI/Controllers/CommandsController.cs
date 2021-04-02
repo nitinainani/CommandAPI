8758802ac55a4124a16268b650655d2b1ac9a3f4
Ainani, Nitin <nitin.ainani@nyhart.com>
2021-04-02 11:47:43 -0400

    Chp6Done

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using CommandAPI.Data;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controller
@@ -7,10 +9,31 @@ namespace CommandAPI.Controller
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase{
-     
+
+        private readonly ICommandAPIRepo _commandAPIRepo;
+
+        public CommandsController(ICommandAPIRepo commandAPIRepo)
+        {
+            _commandAPIRepo = commandAPIRepo;
+        }
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get(){
+        //     return new string[] {"this", "is","hard", "coded"};
+        // }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(){
-            return new string[] {"this", "is","hard", "coded"};
+        public ActionResult<IEnumerable<Command>> GetAllCommands(){
+            var commandItems = _commandAPIRepo.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+         [HttpGet("{Id}")]
+        public ActionResult<Command> GetCommandById([FromRoute] int Id){
+            var commandItems = _commandAPIRepo.GetCommandById(Id);
+            if(commandItems == null){
+                return NotFound();
+            }
+            return Ok(commandItems);
         }
     }
 }
